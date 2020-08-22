f068e43391f28607527ea52a05eaebe4d33bee6c
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2020-08-22 22:40:11 +0530

    add Jwt response

---
+++ src/main/java/com/vaibhav/jwtauth/model/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.vaibhav.jwtauth.model;
+
+public class JwtResponse {
+    private final String jwttoken;
+
+    public JwtResponse(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+
+    public String getJwttoken() {
+        return jwttoken;
+    }
+}
